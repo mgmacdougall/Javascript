ccca69b4f82f00d1f2afba627e14d3d9661d3924
Mike MacDougall <MacD>
2021-09-12 12:18:28 -0400

    US1 JSON data, US: Create Dino Constructor, US: Create All dino Objects

--- app.js
+++ app.js
@@ -1,32 +1,59 @@
+// let dinos = [];
+const getDinoData = async () => {
+  const fetchedData = await fetch("./dino.json");
+  const data = await fetchedData.json();
+  return data.Dinos;
+};
 
-    // Create Dino Constructor
+// Create Dino Constructor
+function Dinos(dino) {
+  const { species, weight, height, diet, where, when, fact } = dino;
+  this.species = species;
+  this.weight = weight;
+  this.height = height;
+  this.diet = diet;
+  this.where = where;
+  this.when = when;
+  this.fact = fact;
+}
 
+// Test Code
+const testData = {
+  species: "Triceratops",
+  weight: 13000,
+  height: 114,
+  diet: "herbavor",
+  where: "North America",
+  when: "Late Cretaceous",
+  fact: "First discovered in 1889 by Othniel Charles Marsh",
+};
 
-    // Create Dino Objects
+// Create Dino Objects
+const createDinos = async() =>{
+    let dinos = await getDinoData();
+    console.log(dinos)
+    dinos.forEach(i => console.log(i))
+    return dinos.map((e)=> new Dinos(e))
+}
 
+createDinos().then((data)=> console.log(data));
+// Create Human Object
 
-    // Create Human Object
+// Use IIFE to get human data from form
 
-    // Use IIFE to get human data from form
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Generate Tiles for each Dino in Array
 
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Add tiles to DOM
 
-
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
-
-    // Remove form from screen
-
+// Remove form from screen
 
 // On button click, prepare and display infographic
